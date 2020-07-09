a82426cfa93a8f3e6e24600dbc422aa6bf2cb81a
guachochi <davis.bradleyj@gmail.com>
2020-07-09 13:07:09 -0700

    04-timer mod drills 02/03/02-stopwatch

--- 24-post-bootcamp/post-content/04-timers/02-code-drills/03-challenging/02-Stopwatch/Unsolved/stopwatch.js
+++ 24-post-bootcamp/post-content/04-timers/02-code-drills/03-challenging/02-Stopwatch/Unsolved/stopwatch.js
@@ -30,20 +30,24 @@ var stopwatch = {
     stopwatch.lap = 1;
 
     //  TODO: Change the "display" div to "00:00."
-
+    $("#display").text("00:00")
+    $("#laps").text("")
   },
 
   start: function() {
 
     //  TODO: Use setInterval to start the count here and set the clock to running.
     if (!clockRunning) {
-
+      clockRunning = true;
+      intervalId = setInterval(stopwatch.count, 1000)
     }
 
   },
   stop: function() {
 
     //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId)
+    clockRunning = false
 
   },
 
@@ -51,20 +55,22 @@ var stopwatch = {
 
     //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
     //        and save the result in a variable.
-
+    var currentTime = stopwatch.timeConverter(stopwatch.time);
     //  TODO: Add the current lap and time to the "laps" div.
-
+    $("#laps").append("<p>Lap: " + stopwatch.lap + " --- " + currentTime + "</p>");
     //  TODO: Increment lap by 1. Remember, we can't use "this" here.
+    stopwatch.lap++;
   },
   count: function() {
 
     //  TODO: increment time by 1, remember we cant use "this" here.
-
+    stopwatch.time++;
     //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
     //        and save the result in a variable.
+    var currentTime = stopwatch.timeConverter(stopwatch.time);
 
     //  TODO: Use the variable you just created to show the converted time in the "display" div.
-
+    $("#display").text(currentTime)
   },
 
   //  THIS FUNCTION IS DONE FOR US!

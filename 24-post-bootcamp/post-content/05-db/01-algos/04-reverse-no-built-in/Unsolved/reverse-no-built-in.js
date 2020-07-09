1da345b01cf71c9d4b95d072aa2ad971fc06eeaa
guachochi <davis.bradleyj@gmail.com>
2020-07-09 13:46:15 -0700

    05-db/01-algos/04-reverse challenge

--- 24-post-bootcamp/post-content/05-db/01-algos/04-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 24-post-bootcamp/post-content/05-db/01-algos/04-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,6 +1,11 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-var reverse = function(str) {
-
+const reverse = function(str) {
+    let revStr = ""
+    for (let i=str.length-1; i>=0; i--) {
+        revLetter = str[i]
+        revStr += revLetter
+    }
+    return revStr
 };

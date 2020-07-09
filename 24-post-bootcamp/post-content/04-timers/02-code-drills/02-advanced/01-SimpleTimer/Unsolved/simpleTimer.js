c99844b874c834efad76f36cd0bad4f659cae2c6
guachochi <davis.bradleyj@gmail.com>
2020-07-09 11:20:17 -0700

    04-timer mod drills 02/02/01-SimpleTimer

--- 24-post-bootcamp/post-content/04-timers/02-code-drills/02-advanced/01-SimpleTimer/Unsolved/simpleTimer.js
+++ 24-post-bootcamp/post-content/04-timers/02-code-drills/02-advanced/01-SimpleTimer/Unsolved/simpleTimer.js
@@ -4,22 +4,30 @@
 //  after 15 seconds, execute the timeUp function
 
 // ---------------------
-// CODE STEP One HERE
+setTimeout(fiveSeconds,5000)
+setTimeout(tenSeconds,10000)
+setTimeout(timeUp,15000)
 // ---------------------
 
 // Step 2:
 // Fill in the blanks to these functions.
 function fiveSeconds() {
   // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  $("#time-left").append("<h2>About 10 Seconds Left!</h2>")
   // console log 10 seconds left
+  console.log("10 seconds left")
 }
 
 function tenSeconds() {
   // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  $("#time-left").append("<h2>About 5 Seconds Left!</h2>")
   // console log 5 seconds left
+  console.log("5 seconds left")
 }
 
 function timeUp() {
   // in the element with an id of `time-left` add an h2 saying Time's Up!
+  $("#time-left").append("<h2>Times Up!</h2>")
   // console log done
+  console.log("Done!")
 }

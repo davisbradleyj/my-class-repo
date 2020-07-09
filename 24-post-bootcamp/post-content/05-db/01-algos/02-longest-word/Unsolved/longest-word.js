2e3689d0d367d8cf816fb46d0f850c0db8faf4f8
guachochi <davis.bradleyj@gmail.com>
2020-07-09 13:42:20 -0700

    05-db/01-algos/02 and 03 challenges

--- 24-post-bootcamp/post-content/05-db/01-algos/02-longest-word/Unsolved/longest-word.js
+++ 24-post-bootcamp/post-content/05-db/01-algos/02-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,15 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+const longestWord = function(str) {
+    let wordList = str.split(" ")
+    let longWord = wordList[0]
+
+    for (let i=0; i<wordList.length; i++) {
+        let currWord = wordList[i]
+        // make sure you are accounting for the length of currWord and longWord
+        if (currWord.length > longWord.length) {
+            longWord = currWord
+        }
+    }
+    return longWord
+};
--- 24-post-bootcamp/post-content/05-db/01-algos/03-acronym-builder/Unsolved/acronym-builder.js
+++ 24-post-bootcamp/post-content/05-db/01-algos/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,15 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function(str) {
+    // acronym will be the first letter of each word
+    // split string
+    let string = str.split(" ")
+    let acronym = ""
+    // loop through each word in each string and store the first letter of each word
+    for (let i=0; i<string.length; i++) {
+        word = string[i]
+        acronym += word[0].toUpperCase();
+    }
+    // return that acronym with each letter toUpperCase
+    return acronym
+};

3a348721881dda3f3278785d4a44736ef5ded872
guachochi <davis.bradleyj@gmail.com>
2020-07-09 12:04:07 -0700

    04-timer mod drills 02/02/03-slideshow

--- 24-post-bootcamp/post-content/04-timers/02-code-drills/02-advanced/03-Slideshow/Unsolved/slideshow.js
+++ 24-post-bootcamp/post-content/04-timers/02-code-drills/02-advanced/03-Slideshow/Unsolved/slideshow.js
@@ -2,7 +2,7 @@
 // Students: follow the instructions below:
 
 // TODO: Put links to our images in this image array.
-var images = ["images/bootstrap.png"];
+var images = ["images/bootstrap.png", "images/github-logo.jpg", "images/logo_JavaScript.png"];
 
 // Variable showImage will hold the setInterval when we start the slideshow
 var showImage;
@@ -11,9 +11,9 @@ var showImage;
 var count = 0;
 
 // TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
-
+$("#start").click(startSlideshow)
 // TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
-
+$("#stop").click(stopSlideshow)
 
 // This function will replace display whatever image it's given
 // in the 'src' attribute of the img tag.
@@ -25,26 +25,28 @@ function displayImage() {
 function nextImage() {
 
   // TODO: Increment the count by 1.
-
+  count++;
 
   // TODO: Show the loading gif in the "image-holder" div.
-
-
-  // TODO: Use a setTimeout to run displayImage after 1 second.
-
+  $("#image-holder").html("<img src='images/loading.gif'/>");
 
   // TODO: If the count is the same as the length of the image array, reset the count to 0.
-
+  if (count === images.length) {
+    count = 0
+  }
+  // TODO: Use a setTimeout to run displayImage after 1 second.
+  setTimeout(displayImage,1000)
+  
 }
 function startSlideshow() {
 
   // TODO: Use showImage to hold the setInterval to run nextImage.
-
+  showImage = setInterval(nextImage,2500)
 }
 function stopSlideshow() {
 
   // TODO: Put our clearInterval here:
-
+  clearInterval(showImage)
 }
 
 // This will run the display image function as soon as the page loads.

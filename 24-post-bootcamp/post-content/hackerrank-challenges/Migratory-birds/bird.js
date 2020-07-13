c1978c6a9900ef789d7e8eea754ea30506c7ad6d
guachochi <davis.bradleyj@gmail.com>
2020-07-13 13:43:03 -0700

    hackerrank challenge complete - Migratory Birds

--- 24-post-bootcamp/post-content/hackerrank-challenges/Migratory-birds/bird.js
+++ 24-post-bootcamp/post-content/hackerrank-challenges/Migratory-birds/bird.js
@@ -1,12 +1,21 @@
 // Given an array of numbers, return the smallest number of the most frequently occuring number
 
+
 function migratoryBirds(arr) {
-    let 
-    if (arr.length = 1) {
-        return arr[0]
-    } else if (arr.length > 1) {
-
-
+    let most = 0
+    let count = most
+    let bird = 0
+    arr.sort()
+    console.log(arr)
+    for (let i=0; i<arr.length; i++) {
+        most = (arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i])) + 1;
+        if (most > count) {
+            count = most;
+            bird = arr[i]
+            console.log(bird)
+        }
     }
-
+    return bird
 }
+
+console.log(migratoryBirds([1,2,3,4,5,4,3,2,1,3,4]))

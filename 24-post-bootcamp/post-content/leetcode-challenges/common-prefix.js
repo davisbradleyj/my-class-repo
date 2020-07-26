3b86aa7fa3bfe97541717bd4f3d206146b4c2806
guachochi <davis.bradleyj@gmail.com>
2020-07-26 14:12:38 -0700

    leetcode challenge - pseudocode comments for common prefix

--- 24-post-bootcamp/post-content/leetcode-challenges/common-prefix.js
+++ 24-post-bootcamp/post-content/leetcode-challenges/common-prefix.js
@@ -3,20 +3,25 @@
 // If there is no common prefix, return an empty string "".
 
 var longestCommonPrefix = function(strs) {
+    // set the prefix as an empty string
     let pre = ""
+    // set condition to return an empty strings if there is no set of strings given
     if (strs === null || strs.length === 0) {
         return pre
     }
+    // loop through first string in array
     for (let i=0; i<strs[0].length; i++) {
+        // set a variable to hold the letter in the first word, if all corresponding words have that same letter
         let str1 = strs[0][i]
-        console.log("Str1: ",str1)
+        // loop through remaining words to check if they contain the same characters in order as compared to str1
         for (let j=1; j<strs.length; j++) {
+            // when a word is found not to have the same prefix, return pre
             if (strs[j][i] !== str1) {
                 return pre
             }
         } 
+        // build onto pre with the letters common to all elements in the strs array
         pre = pre + str1
-        console.log("Pre: ",pre)
     }
     return pre
 };

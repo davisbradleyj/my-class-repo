57b21ad4ae687caaa0e2bed0f37a26fab13ada7a
guachochi <davis.bradleyj@gmail.com>
2020-07-26 14:19:39 -0700

    leetcode challenge - toLowercase using method

---
+++ 24-post-bootcamp/post-content/leetcode-challenges/to-lower-case.js
@@ -0,0 +1,9 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+var toLowerCase = function(str) {
+    let newStr = str.toLowerCase()
+    return newStr
+};
+
+// --- or ---
+

99512cdceab9c301e35ecfd62744513e78eec856
guachochi <davis.bradleyj@gmail.com>
2020-07-28 10:35:54 -0700

    codewars - review high-low solution

--- 24-post-bootcamp/post-content/codewars/high-low.js
+++ 24-post-bootcamp/post-content/codewars/high-low.js
@@ -1,16 +1,16 @@
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 function highAndLow(numbers){
-    let high = numbers[0]
-    let low = numbers[0]
+    let high = parseInt(numbers[0])
+    let low = parseInt(numbers[0])
     for (let i=1; i<numbers.length; i++) {
-        if (numbers[i] > high) {
-            high = numbers[i]
+        if (parseInt(numbers[i]) > high) {
+            high = parseInt(numbers[i])
         }
-        if (numbers[i] < low) {
-            low = numbers[i]
+        if (parseInt(numbers[i]) < low) {
+            low = parseInt(numbers[i])
         }
     }
-    return "'" + high + " " + low + "'"
+    return high+" "+low
 }
 console.log(highAndLow([4,5,29,54,4,0,-214,542,-64,1,-3,6,-6]))

121bbfc3818d5a3ff20c2e102feb5f8c40a21e15
guachochi <davis.bradleyj@gmail.com>
2020-08-16 12:48:37 -0700

    complete LC challenge - happy number

---
+++ 24-post-bootcamp/post-content/leetcode-challenges/happy-number.js
@@ -0,0 +1,27 @@
+// Write an algorithm to determine if a number n is "happy".
+
+// A happy number is a number defined by the following process: 
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+// Those numbers for which this process ends in 1 are happy numbers.
+
+// Return True if n is a happy number, and False if not.
+
+var isHappy = function(n) {
+    if (n === 1) {
+        return true
+    }
+    else if (n === 4) { // for all tests, if the sum of digits resulted in 4, the stack would overflow
+        return false
+    }
+    else {
+        let num = n.toString().split('').reduce((acc,curr) => {
+            acc += curr*curr
+            return acc
+        }, 0)
+        console.log(num)
+        return isHappy(num)
+    }
+};
+
+console.log(isHappy(40))

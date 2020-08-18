4fc8a22ab2980931970893e7962290d7541ab4e1
guachochi <davis.bradleyj@gmail.com>
2020-08-18 13:42:24 -0700

    module 24/unit 13/01 python/01 drills/js/02-core

--- 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/02-core-dog-api/challenge-prompt.js
+++ 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/02-core-dog-api/challenge-prompt.js
@@ -4,9 +4,7 @@ $(document).submit(function () {
 
     var dogSearch;
     // grab the value of the users search and store it in dogSearch
-
-
-
+    dogSearch = $('#dogSearch').val()
 
     // We are defining the query URL here. By concatinating strings together, we're able to change our query based upon user input
     var queryURL = "https://dog.ceo/api/breed/" + dogSearch  + "/images/random";
@@ -18,10 +16,10 @@ $(document).submit(function () {
             method: "GET"
         }).then(function (response) {
             // console.log the response 
-            
+            console.log(response)
             
             // append the image within the response to the page 
-
+            $('.dogImage').append("<img src=" + response.message + ">")
         });
     }
 })

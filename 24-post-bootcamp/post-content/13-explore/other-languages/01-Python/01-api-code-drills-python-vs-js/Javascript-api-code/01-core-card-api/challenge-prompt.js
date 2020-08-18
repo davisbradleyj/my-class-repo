72a6800bcff4067c814b2ce21df7c7cf100a1bb9
guachochi <davis.bradleyj@gmail.com>
2020-08-18 13:35:57 -0700

    module 24/unit 13/01 python/01 drills/js/01-core

--- 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/01-core-card-api/challenge-prompt.js
+++ 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/01-core-card-api/challenge-prompt.js
@@ -8,10 +8,11 @@ $(document).submit(function () {
         method: "GET"
     }).then(function (response) {
         // console.log the response 
-
+        console.log(response)
         // console.log the remaining cards
-
+        console.log(response.remaining)
         // console.log the deck id too
+        console.log(response.deck_id)
 
     });
 

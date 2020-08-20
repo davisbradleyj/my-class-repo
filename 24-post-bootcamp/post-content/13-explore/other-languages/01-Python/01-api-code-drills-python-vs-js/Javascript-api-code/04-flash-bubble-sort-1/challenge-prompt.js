1b9534c5f7015c3b6e1b8a215d9f8edcdd28b5c6
guachochi <davis.bradleyj@gmail.com>
2020-08-20 12:31:25 -0700

    module 24/unit 13/01 python/01 drills/js/04-flash & 05-flash

--- 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/04-flash-bubble-sort-1/challenge-prompt.js
+++ 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/04-flash-bubble-sort-1/challenge-prompt.js
@@ -2,17 +2,19 @@
 // highest value to lowest value and returns the new sorted array
 
 function bubbleSort(arr) {
-// ================= code goes here ===========================
+  // ================= code goes here ===========================
+  for (var i = arr.length - 1; i >= 0; i--) {
+    for (var j = 1; j <= i; j++) {
+      if (arr[j - 1] < arr[j]) {
+        var temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr
 
-
-
-
-
-
-
-
-
-// ============================================================
+  // ============================================================
 }
 
 var testArray = [34, 23, 3, 76, 20, 84, 18, 9];
--- 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/05-flash-bubble-sort-2/challenge-prompt.js
+++ 24-post-bootcamp/post-content/13-explore/other-languages/01-Python/01-api-code-drills-python-vs-js/Javascript-api-code/05-flash-bubble-sort-2/challenge-prompt.js
@@ -8,23 +8,25 @@
 
 
 function bubbleSortTwo(arr) {
-    // ================= code goes here ===========================
-
-    
-
-
-
-
-
-
-
-
-
-    
-    // ============================================================
+  // ================= code goes here ===========================
+  let status = true
+  while (status) {
+    status = false
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        status = true;
+      }
+    }
+  }
+  return arr
+  // ============================================================
 }
-
 
 var age = [34, 23, 3, 76, 20, 84, 18, 9];
 var newAge = bubbleSortTwo(age);
 console.log(newAge)
+
+

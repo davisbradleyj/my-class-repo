b07bccbdc263a8bdd544992968c5175bd00b5f66
guachochi <davis.bradleyj@gmail.com>
2020-08-23 17:47:43 -0700

    compete LC challenge - contains-dupe

---
+++ 24-post-bootcamp/post-content/leetcode-challenges/contains-dupe.js
@@ -0,0 +1,24 @@
+// Given an array of integers, find if the array contains any duplicates.
+
+// Your function should return true if any value appears at least twice in the array, 
+// and it should return false if every element is distinct.
+
+var containsDuplicate = function (nums) {
+  nums.sort(function (a, b) { return a - b })
+  console.log(nums)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.length == 0) {
+      console.log('length 0')
+      return false
+    } else if (nums[i] == nums[i+1]) {
+      console.log('nums i:', nums[i])
+      return true
+    }
+  }
+  return false
+};
+
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+console.log(containsDuplicate([1, 4, 3, 2]))
+console.log(containsDuplicate([2, 14, 18, 22, 22]))
+console.log(containsDuplicate([]))

c47d323b41fee477a20a7f1de35a9f68b7592ca7
guachochi <davis.bradleyj@gmail.com>
2020-10-06 12:50:31 -0700

    complete HR challenge - between-two-sets

--- 24-post-bootcamp/post-content/hackerrank-challenges/Migratory-birds/bird.js
+++ 24-post-bootcamp/post-content/hackerrank-challenges/Migratory-birds/bird.js
@@ -5,7 +5,7 @@ function migratoryBirds(arr) {
     let most = 0
     let count = most
     let bird = 0
-    arr.sort()
+    arr.sort(a,b => (a-b))
     console.log(arr)
     for (let i=0; i<arr.length; i++) {
         most = (arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i])) + 1;
@@ -16,6 +16,30 @@ function migratoryBirds(arr) {
         }
     }
     return bird
+}
+
+console.log(migratoryBirds([1,2,3,4,5,4,3,2,1,3,4]))
+
+
+function migrantBirds(arr){
+    arr.sort(function (a, b) { return a - b})
+    const numMap = {};
+    var maxNum = 0;
+    var maxChar = 0;
+    for (var num of arr) { 
+        if (numMap[num]) {
+            numMap[num]++;
+        } else { 
+            numMap[num] = 1;
+        }
+    }
+    for (var num in numMap) { 
+        if (numMap[num] > maxNum) { 
+            maxNum = numMap[num];
+            maxChar = num;
+        }
+    }
+    return maxChar;
 }
 
 console.log(migratoryBirds([1,2,3,4,5,4,3,2,1,3,4]))

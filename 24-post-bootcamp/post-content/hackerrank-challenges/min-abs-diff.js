bf9dd781bb2932a403555b47c4dad09e198c6d77
guachochi <davis.bradleyj@gmail.com>
2020-10-12 21:37:29 -0700

    complete HR challenge - minimum absolute difference

---
+++ 24-post-bootcamp/post-content/hackerrank-challenges/min-abs-diff.js
@@ -0,0 +1,28 @@
+function minimumAbsoluteDifference(arr) {
+  arr.sort((a,b) => a-b)
+  console.log(arr)
+  let hold = arr[arr.length-1] - arr[0] 
+  for (let i=0; i<arr.length; i++) {
+    for (let j=i+1; j<arr.length; j++) {
+      let temp = arr[j] - arr[i]
+      console.log(temp)
+      if (temp<hold) {
+        hold = temp
+        console.log(hold)
+      }
+    }
+  }
+  return hold 
+}
+
+console.log(minimumAbsoluteDifference([1,-3,71,68,17]))
+console.log(minimumAbsoluteDifference([-59,-36,-13,1,-53,-92,-2,-96,-54,75]))
+
+function minAbsDiff(arr) {
+  minDiff = arr[arr.length-1] - arr[0] 
+  arr.sort(function(a, b) {
+    minDiff = Math.min(minDiff, Math.abs(a - b));
+    return a - b;
+  });
+  return minDiff;
+}

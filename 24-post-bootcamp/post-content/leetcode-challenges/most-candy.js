4d88885ad78d32af88d36c078ecb034e9c90ce89
guachochi <davis.bradleyj@gmail.com>
2020-12-26 14:13:37 -0800

    Minor refactor of code to experiment with efficiency useing spread and sort/pop methods

--- 24-post-bootcamp/post-content/leetcode-challenges/most-candy.js
+++ 24-post-bootcamp/post-content/leetcode-challenges/most-candy.js
@@ -18,3 +18,19 @@ var kidsWithCandies = function (candies, extraCandies) {
 
 console.log(kidsWithCandies([2,3,5,1,3],3));
 console.log(kidsWithCandies([4,2,1,1,2],1));
+
+var refactor = function (candies, extraCandies) {
+  let highest = [...candies];
+  let output = [];
+  highest = highest.sort((a,b) => a-b).pop()
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= highest) {
+      output.push(true);
+    } else {
+      output.push(false);
+    }
+  }
+  return output;
+}
+
+console.log(refactor([2,3,5,1,3],3));

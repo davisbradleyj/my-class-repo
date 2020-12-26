e6de982c693cfc48fcab987d87d448c979b3516d
guachochi <davis.bradleyj@gmail.com>
2020-12-26 14:06:52 -0800

    Complete LC challenge - most-candy.js

---
+++ 24-post-bootcamp/post-content/leetcode-challenges/most-candy.js
@@ -0,0 +1,20 @@
+var kidsWithCandies = function (candies, extraCandies) {
+  let highest = 0;
+  let output = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] >= highest) {
+      highest = candies[i]
+    }
+  }
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= highest) {
+      output.push(true);
+    } else {
+      output.push(false);
+    }
+  }
+  return output;
+};
+
+console.log(kidsWithCandies([2,3,5,1,3],3));
+console.log(kidsWithCandies([4,2,1,1,2],1));

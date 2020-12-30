946fb9823c021155b55e010df21d5e25be79be19
guachochi <davis.bradleyj@gmail.com>
2020-12-30 13:43:26 -0800

    Complete Node/code drills/14 activity

--- 09-NodeJS/code-drills/00-required-code-drills/14-CommandSort/Unsolved/commandSortUnsolved.js
+++ 09-NodeJS/code-drills/00-required-code-drills/14-CommandSort/Unsolved/commandSortUnsolved.js
@@ -2,3 +2,16 @@
 // Create an application that takes in a series of numbers then sorts them.
 // Feel encouraged to use Stack or Google to find the "sort" code.
 // ===========================================================================================
+
+let args = process.argv;
+let array = [];
+for (let i = 2; i < args.length; i++) {
+  array.push(parseFloat(args[i]))
+}
+
+console.log(array.sort(numSort))
+
+function numSort(a,b) {
+  return (a-b)
+}
+

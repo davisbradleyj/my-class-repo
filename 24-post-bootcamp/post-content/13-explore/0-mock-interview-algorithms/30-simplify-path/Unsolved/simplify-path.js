25f70fcbac9d70bf7318ad9f384a3b1526ba2d29
guachochi <davis.bradleyj@gmail.com>
2021-01-15 09:34:10 -0800

    Complete postbc algo challenge 30-simplify-path

--- 24-post-bootcamp/post-content/13-explore/0-mock-interview-algorithms/30-simplify-path/Unsolved/simplify-path.js
+++ 24-post-bootcamp/post-content/13-explore/0-mock-interview-algorithms/30-simplify-path/Unsolved/simplify-path.js
@@ -1,3 +1,26 @@
 // Write a function that takes in an absolute file path and simplifies it
 
-var simplifyPath = function(path) {};
+var simplifyPath = function(path) {
+  let arr = path.split("/")
+  console.log(arr)
+  let str = []
+  for (let i = 0; i<arr.length; i++) {
+    if (!arr[i]){
+      continue;
+    }
+    if (arr[i] === ".") {
+      continue;
+    }
+    if (arr[i] === "..") {
+      str.pop();
+      continue
+    }
+    str.push(arr[i])
+    // console.log(str)
+  }
+  str = str.join("/");
+  console.log("/" + str)
+  return `/${str}`
+};
+
+console.log(simplifyPath('/a//b////c/d//././/..'))
